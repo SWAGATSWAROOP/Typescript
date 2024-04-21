528c3d947ec9610e38726cf126c7285357eca4c5
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-04-22 02:36:18 +0530

    Add getHello function and handle errors in function.ts

--- function.ts
+++ function.ts
@@ -31,6 +31,33 @@ console.log(getValue(5))
 console.log(getValue(3))
 signUpUser("swagat","swagat@gmail.com","Swagat@1234",true);
 
+const getHello = (s:string):string => {
+    return ""
+}
+
+console.log(getHello("Swagat"))
+
+const heros = [1,"spiderman","ironman",true];
+
+// Context switching 
+// returning a string
+heros.map((hero ): string => {
+    return `hero is ${hero}`
+})
+
+function consoleError(errmas:string):void{
+    console.log(errmas)
+}
+
+// For handling error and not crashing the website we should use never
+// For Error execution and termination never is used
+function handleError(errmas:string):never{
+    throw new Error(errmas)
+}
+
+
+
+
 
 
 export {}
