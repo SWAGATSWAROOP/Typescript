b6993edc6cbdb698aa1d3154594aca327f752817
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-04-22 22:01:02 +0530

    Access Modifiers

--- production typescript/src/index.ts
+++ production typescript/src/index.ts
@@ -25,4 +25,43 @@ class User{
 const swagat = new User("Swagat@gmail.com","Swagat");
 
 // Professional People write code in this way
+class user{
+    readonly city: string = "Cuttuck"
+    protected coursecount = 1;
+    constructor(public name:string,public email:string){
+        this.email = email;
+        this.name = name;
+    }
+
+    // getter for email
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCOUNT():number{
+        return this.coursecount;
+    }
+
+    // In setter there should not be any return type
+    set courseCount(coursenum:number){
+        if(coursenum <= 1){
+            throw new Error("Course count should be more than 1")
+        }
+        this.coursecount = coursenum;
+    }
+
+    private deleteToken(){
+        console.log("Swagat");
+    }
+}
+
+
+class SubUser extends user{
+    isFamily:boolean = true
+    changeCourseCount(){
+        this.coursecount;
+    }
+}
+
+
 
