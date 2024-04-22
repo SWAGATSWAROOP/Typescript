3ee201a029033d928c839d1fdc3ee63d9009a9d5
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-04-22 14:32:27 +0530

    Refactor User class in index.ts and index.js

--- production typescript/dist/index.js
+++ production typescript/dist/index.js
@@ -4,3 +4,15 @@ console.log("Hello");
 function Swagat(num) {
     return num;
 }
+// Classes
+class User {
+    // But we can declare private also in other way
+    // #name : string in JS
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Delhi";
+    }
+}
+const swagat = new User("Swagat@gmail.com", "Swagat");
+// Professional People write code in this way
--- production typescript/src/index.ts
+++ production typescript/src/index.ts
@@ -5,3 +5,24 @@ console.log("Hello")
 function Swagat(num:number):number{
     return num;
 }
+
+// Classes
+
+class User{
+    // We need to predefine all the variables
+    email : string
+    name : string
+    private readonly city : string
+    // But we can declare private also in other way
+    // #name : string in JS
+    constructor(email:string,name:string){
+        this.email = email;
+        this.name = name;
+        this.city = "Delhi"
+    }
+}
+
+const swagat = new User("Swagat@gmail.com","Swagat");
+
+// Professional People write code in this way
+
